0b641a83bba0c6251835810c9f934d11b2639eb7
wlt <734490921@qq.com>
2022-06-11 10:07:55 +0800

    完善了主页

---
+++ assets/js/baseAPI.js
@@ -0,0 +1,27 @@
+// 注意：每次调用$.get()或$.post()或$.ajax()的时候
+// 会先调用ajaxprefilter 这个函数
+// 在这个函数中，可以拿到我们给Ajax提供的配置对象
+$.ajaxPrefilter((options) => {
+  // console.log(options)
+  // 在请求之前拼接上根路径
+  options.url = 'http://www.liulongbin.top:3007' + options.url
+  // 注入 token
+  if (options.url.includes('/my/')) {
+    options.headers = {
+      Authorization: localStorage.getItem('token'),
+    }
+  }
+  // 权限校验
+  options.complete = (res) => {
+    // console.log(res);
+    if (
+      res.responseJSON.status === 1 &&
+      res.responseJSON.message === "身份认证失败！"
+    ) {
+      //1. 强制清空 token
+      localStorage.removeItem('token')
+      //2. 跳转到登录页面
+      location.href = '/login.html'
+    }
+  }
+})
---
+++ assets/js/index.js
@@ -0,0 +1,58 @@
+function getUserInfo() {
+  $.ajax({
+    method: 'GET',
+    url: '/my/userinfo',
+    headers: {
+      Authorization: localStorage.getItem('token'),
+    },
+    success: (res) => {
+      //   console.log(res)
+      if (res.status !== 0) return layer.msg(res.message)
+      layer.msg('获取用户信息成功')
+      // 调用渲染函数
+      renderAvatar(res.data)
+    },
+
+    // // 权限校验
+    // complete: (res) => {
+    //   // console.log(res);
+    //   if (
+    //     res.responseJSON.status === 1 &&
+    //     res.responseJSON.message === "身份认证失败！"
+    //   ) {
+    //     //1. 强制清空 token
+    //     localStorage.removeItem('token')
+    //     //2. 跳转到登录页面
+    //     location.href = '/login.html'
+    //   }
+    // },
+  })
+}
+
+// 渲染用户信息
+const renderAvatar = (user) => {
+  const name = user.nickname || user.username
+  // 渲染欢迎语
+  $('#welcome').html(`欢迎 ${name}`)
+  // 按需渲染头像
+  if (user.user_pic !== null) {
+    $('.layui-nav-img').attr('src', user_pic).show()
+    $('.text-avatar').hide()
+  } else {
+    $('.layui-nav-img').hide()
+    let first = name[0].toUpperCase()
+    $('.text-avatar').html(first).show()
+  }
+}
+
+// 退出登录
+$('#btnLogout').click(() => {
+  layer.confirm('是否退出登录？', { icon: 3, title: '提示' }, function (index) {
+    // 1.清除本地存储的token
+    localStorage.removeItem('token')
+    // 2.跳转到登录页面
+    location.href = '/login.html'
+  })
+})
+// 获取用户列表
+getUserInfo()
